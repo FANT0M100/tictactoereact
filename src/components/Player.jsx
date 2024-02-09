536687dd5c880c1c048597b94ec34b0fb76c270c
FANT0M100 <giorgie103188@gmail.com>
2024-02-09 19:54:33 +0400

    change player name and end

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
-const Player = ({ initialName, symbol, isActive }) => {
+const Player = ({ initialName, symbol, isActive, onChangeName }) => {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((prevState) => !prevState);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(e) {
